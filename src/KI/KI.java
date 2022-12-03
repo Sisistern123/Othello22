019e4907400485e4caa3b9238c35cf90516deaff
Sisistern123 <selin.t@live.de>
2022-12-03 13:40:31 +0100

    KI

--- src/KI/KI.java
+++ src/KI/KI.java
@@ -7,11 +7,18 @@ import szte.mi.Player;
 import java.util.Random;
 
 public class KI implements Player {
-
     private Othello othello;
     private int player;
     private int opponent;
     private int currentPlayer = 1;
+
+
+    public KI(Othello othello, int order, Random rando, long t) {
+        this.othello = othello;
+        init(order, t, rando);
+
+    }
+
 
     @Override
     public void init(int order, long t, Random rnd) {
@@ -30,10 +37,22 @@ public class KI implements Player {
 
     @Override
     public Move nextMove(Move prevMove, long tOpponent, long t) {
+
         if(othello.checkLegalMoves(currentPlayer, prevMove)) {
             othello.directionFlip(currentPlayer, prevMove);
-            currentPlayer = opponent;
+            switchCurrentPlayer();
         }
-        return null;
+        othello.calcLegalMoves(currentPlayer);
+        Random r = new Random(othello.getLegalMoves().size()-1); //random move from legalMoves ArrayList
+
+        return othello.getLegalMoves().get(r.nextInt());
+    }
+
+    public void switchCurrentPlayer() { //falls der derzeitige spieler black war, wurde er danach zu white gesetzt
+        if(currentPlayer == this.player) {
+            this.currentPlayer = this.opponent;
+        } else if(currentPlayer == this.opponent) {
+            this.currentPlayer = this.player;
+        }
     }
 }
